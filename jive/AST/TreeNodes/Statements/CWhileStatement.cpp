af17d49b17c99f94b002c2585401bb90bf4facf1
alexeyqu <alexeyqu@gmail.com>
2016-11-19 14:27:24 +0300

    cleanup

--- jive/AST/TreeNodes/Statements/CWhileStatement.cpp
+++
@@ -1,8 +0,0 @@
-#include "CWhileStatement.h"
-
-CWhileStatement::CWhileStatement( IExpression *_expression, IStatement *_loopStatement ) : \
-	expression( _expression ), loopStatement( _loopStatement ) {}
- 
-void CWhileStatement::Accept( IVisitor *visitor ) {
-	visitor->Visit(this);
-}
