180f117abd3693db7efafe23b6487b58bbf106ea
Nikita Rykov <nikita23angarsk@gmail.com>
2016-11-24 22:40:42 +0300

    Added class table

---
+++ jive/include/ST/CUserClassTable.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+
+class CClass;
+class CIdExpression;
+
+class CUserClassTable
+{
+public:
+   CUserClassTable() {}
+
+   void Clear();
+   void Insert( CClass* userClass );
+   CClass *Lookup( CIdExpression *id ) 
+
+   std::unordered_map< std::string, CClass* > table;
+};  
