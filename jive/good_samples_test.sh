25b588b77f420478bd4a67eb6b5e4e2577197b37
Alexey Kulikov <alexeyqu@gmail.com>
2017-06-27 15:32:07 +0300

    testing environment for synterrors added

---
+++ jive/good_samples_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FOLDER=test
+
+cd $FOLDER
+
+mkdir -p ./good_results
+
+cd ./good_samples
+
+for f in *.java
+do
+    echo $f
+    mkdir -p ../good_results/$f
+    cp $f ../good_results/$f/code.java
+    ../../jive $f ../good_results/$f/
+    # dot -Tps ../good_results/$f/AST_AbstractSyntaxTree.dot -o ../good_results/$f/AST_AbstractSyntaxTree.ps
+    # dot -Tpng ../good_results/$f/AST_AbstractSyntaxTree.dot -o ../good_results/$f/AST_AbstractSyntaxTree.png
+done
--- jive/synterrors_samples_test.sh
+++ jive/synterrors_samples_test.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 FOLDER=test
-# FOLDER=test/synterrors
 
 cd $FOLDER
 
-mkdir -p ./good_results
+mkdir -p ./synterrors_results
 
-cd ./good_samples
+cd ./synterrors_samples
 
 for f in *.java
 do
     echo $f
-    mkdir -p ../good_results/$f
-    cp $f ../good_results/$f/code.java
-    ../../jive $f ../good_results/$f/
+    mkdir -p ../synterrors_results/$f
+    cp $f ../synterrors_results/$f/code.java
+    ../../jive $f ../synterrors_results/$f/
     # dot -Tps ../good_results/$f/AST_AbstractSyntaxTree.dot -o ../good_results/$f/AST_AbstractSyntaxTree.ps
     # dot -Tpng ../good_results/$f/AST_AbstractSyntaxTree.dot -o ../good_results/$f/AST_AbstractSyntaxTree.png
 done
